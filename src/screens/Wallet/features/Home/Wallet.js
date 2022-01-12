386f81679c2174b57aa6986aefa99e034b21ed67
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-01-12 18:20:30 +0700

    Fake remove check consolidate

--- src/screens/Wallet/features/Home/Wallet.js
+++ src/screens/Wallet/features/Home/Wallet.js
@@ -4,14 +4,14 @@ import Extra from '@screens/Wallet/features/Home/Wallet.extra';
 import Followed from '@screens/Wallet/features/Home/Wallet.followed';
 import withNews from '@screens/News/News.enhance';
 import { compose } from 'recompose';
-import StreamLineBottomBar from '@screens/Streamline/features/StreamLineBottomBar';
+// import StreamLineBottomBar from '@screens/Streamline/features/StreamLineBottomBar';
 
 const Wallet = React.memo(() => {
   return (
     <>
       <Extra />
       <Followed />
-      <StreamLineBottomBar />
+      {/* <StreamLineBottomBar /> */}
     </>
   );
 });
