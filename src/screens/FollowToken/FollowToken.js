298758229066d73a739ae70bf5ace7d2f4df09cb
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-05-13 17:18:29 +0700

    Improve perf (#2611)
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    Co-authored-by: TN <thu@incognito.org>

--- src/screens/FollowToken/FollowToken.js
+++ src/screens/FollowToken/FollowToken.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, TouchableOpacity, Text3 } from '@src/components/core';
 import { View2 } from '@src/components/core/View';
 import globalStyled from '@src/theme/theme.styled';
 import Header from '@src/components/Header';
-import {TokenBasic as Token, ListAllToken, TokenFollow} from '@src/components/Token';
+import {
+  TokenBasic as Token,
+  ListAllToken2,
+  TokenFollow,
+ handleFilterTokenByKeySearch } from '@src/components/Token';
+
 import PropTypes from 'prop-types';
 import routeNames from '@src/router/routeNames';
 import { useNavigation } from 'react-navigation-hooks';
 import { FONT } from '@src/styles';
 import AddSolidIcon from '@components/Icons/icon.addSolid';
+import { availableTokensSelector } from '@src/redux/selectors/shared';
+import {isEmpty} from 'lodash';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  actionAddFollowToken,
+  actionRemoveFollowToken,
+} from '@src/redux/actions/token';
 import { styled } from './FollowToken.styled';
-import withFollowToken from './FollowToken.enhance';
 
 const AddManually = () => {
   const title = 'Don\'t see your coin?';
@@ -46,16 +57,86 @@ export const Item = ({ item, handleToggleFollowToken }) =>
   }, [item?.isFollowed]);
 
 const FollowTokenList = React.memo((props) => {
-  const { handleToggleFollowToken, ...rest } = props;
+  console.log(props);
+
+  const dispatch = useDispatch();
+
+  const handleToggleFollowToken = async (token) => {
+    try {
+      if (!token?.isFollowed) {
+        dispatch(actionAddFollowToken(token?.tokenId));
+      } else {
+        dispatch(actionRemoveFollowToken(token?.tokenId));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const availableTokens =
+    props?.availableTokens || useSelector(availableTokensSelector);
+
+  const _verifiedTokens = availableTokens?.filter(
+    (token) => token?.isVerified || token?.verified,
+  );
+  const _unVerifiedTokens = availableTokens?.filter(
+    (token) => !token.isVerified || !token.verified,
+  );
+
+  const [verifiedTokens, setVerifiedTokens] = useState(_verifiedTokens);
+
+  const [unverifiedTokens, setUnVerifiedTokens] = useState(_unVerifiedTokens);
+
+  const tokens = [
+    {
+      data: verifiedTokens,
+      visible: true,
+      styledListToken: { paddingTop: 0 },
+    },
+    {
+      data: unverifiedTokens,
+      visible: true,
+      styledListToken: { paddingTop: 15 },
+    },
+  ];
+
+  const handleFilterToken = (searchValue) => {
+    if(!isEmpty(searchValue)) {
+      const __verifiedTokens = handleFilterTokenByKeySearch({
+        tokens: _verifiedTokens,
+        keySearch: searchValue,
+      });
+      const __unVerifiedTokens = handleFilterTokenByKeySearch({
+        tokens: _unVerifiedTokens,
+        keySearch: searchValue,
+      });
+      setVerifiedTokens(__verifiedTokens);
+      setUnVerifiedTokens(__unVerifiedTokens);
+    } else {
+      setVerifiedTokens(_verifiedTokens);
+      setUnVerifiedTokens(_unVerifiedTokens);
+    }
+  };
+
   return (
     <View2 style={styled.container}>
-      <Header title="Add a coin" canSearch titleStyled={FONT.TEXT.incognitoH4} />
-      <View borderTop style={[{ flex : 1 }]}>
-        <ListAllToken
-          {...rest}
+      <Header
+        title="Add a coin"
+        canSearch
+        titleStyled={FONT.TEXT.incognitoH4}
+        isNormalSearch
+        onTextSearchChange={(value) => handleFilterToken(value)}
+      />
+      <View borderTop style={[{ flex: 1 }]}>
+        <ListAllToken2
+          tokensFactories={tokens}
           styledCheckBox={globalStyled.defaultPaddingHorizontal}
           renderItem={({ item }) => (
-            <TokenFollow item={item} handleToggleFollowToken={handleToggleFollowToken} onPress={() => handleToggleFollowToken(item)} />
+            <TokenFollow
+              item={item}
+              handleToggleFollowToken={handleToggleFollowToken}
+              onPress={() => handleToggleFollowToken(item)}
+            />
           )}
         />
       </View>
@@ -64,9 +145,4 @@ const FollowTokenList = React.memo((props) => {
   );
 });
 
-FollowTokenList.propTypes = {
-  handleToggleFollowToken: PropTypes.func.isRequired,
-  tokensFactories: PropTypes.array.isRequired,
-};
-
-export default withFollowToken(FollowTokenList);
+export default FollowTokenList;
--- src/screens/Shield/Shield.js
+++ src/screens/Shield/Shield.js
@@ -1,36 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@src/components/Header';
 import { BtnQuestionDefault } from '@src/components/Button';
 import PropTypes from 'prop-types';
-import {ListAllToken, TokenFollow} from '@src/components/Token';
+import {ListAllToken2, TokenFollow, handleFilterTokenByKeySearch } from '@src/components/Token';
 import { View } from '@components/core';
 import globalStyled from '@src/theme/theme.styled';
 import { FONT } from '@src/styles';
 import { compose } from 'recompose';
 import withLazy from '@components/LazyHoc/LazyHoc';
+import { isEmpty } from 'lodash';
+
 import { styled } from './Shield.styled';
 import withShield from './Shield.enhance';
 
 const Shield = (props) => {
-  const { handleShield, handleWhyShield, hideBackButton, ...rest } = props;
+  const { handleShield, handleWhyShield, hideBackButton, availableTokens, ...rest } = props;
+  
+   const _verifiedTokens = availableTokens?.filter(
+     (token) => token?.isVerified || token?.verified,
+   );
+
+   const _unVerifiedTokens = availableTokens?.filter(
+     (token) => !token.isVerified || !token.verified,
+   );
+
+   const [verifiedTokens, setVerifiedTokens] = useState(_verifiedTokens);
+
+   const [unverifiedTokens, setUnVerifiedTokens] = useState(_unVerifiedTokens);
+
+   const tokens = [
+     {
+       data: verifiedTokens,
+       visible: true,
+       styledListToken: { paddingTop: 0 },
+     },
+     {
+       data: unverifiedTokens,
+       visible: true,
+       styledListToken: { paddingTop: 15 },
+     }];
+
+   const handleFilterToken = (searchValue) => {
+     if (!isEmpty(searchValue)) {
+       const __verifiedTokens = handleFilterTokenByKeySearch({
+         tokens: _verifiedTokens,
+         keySearch: searchValue,
+       });
+       const __unVerifiedTokens = handleFilterTokenByKeySearch({
+         tokens: _unVerifiedTokens,
+         keySearch: searchValue,
+       });
+       setVerifiedTokens(__verifiedTokens);
+       setUnVerifiedTokens(__unVerifiedTokens);
+     } else {
+       setVerifiedTokens(_verifiedTokens);
+       setUnVerifiedTokens(_unVerifiedTokens);
+     }
+   };
+
   return (
     <>
       <Header
         title="Search coins"
         canSearch
+        isNormalSearch
+        onTextSearchChange={(value) => handleFilterToken(value)}
         titleStyled={FONT.TEXT.incognitoH4}
         hideBackButton={hideBackButton}
-        rightHeader={(
+        rightHeader={
           <BtnQuestionDefault
             style={{ marginLeft: 8 }}
             onPress={handleWhyShield}
             customStyle={styled.rightItem}
           />
-        )}
+        }
       />
       <View borderTop style={styled.container}>
-        <ListAllToken
-          {...rest}
+        <ListAllToken2
+          tokensFactories={tokens}
           styledCheckBox={globalStyled.defaultPaddingHorizontal}
           renderItem={({ item }) => (
             <TokenFollow
