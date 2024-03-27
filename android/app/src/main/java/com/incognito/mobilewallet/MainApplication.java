5a859b0cc51baab923234355176bf88f949db7b2
peter <peter@incognito.org>
2024-03-27 14:20:03 +0700

    Modify build android folder

---
+++ android/app/src/main/java/com/incognito/mobilewallet/MainApplication.java
@@ -0,0 +1,82 @@
+package com.incognito.mobilewallet;
+
+import com.incognito.mobilewallet.generated.BasePackageList;
+import android.app.Application;
+
+import java.util.Arrays;
+
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+
+import com.microsoft.codepush.react.CodePush;
+
+public class MainApplication extends Application implements ReactApplication {
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
+  private final ReactNativeHost mReactNativeHost =
+          new ReactNativeHost(this) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+              return BuildConfig.DEBUG;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+              @SuppressWarnings("UnnecessaryLocalVariable")
+              List<ReactPackage> packages = new PackageList(this).getPackages();
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              packages.add(new RNFirebaseAuthPackage());
+              packages.add(new RNFirebaseDatabasePackage());
+              packages.add(new RNFirebaseMessagingPackage());
+              packages.add(new RNFirebaseNotificationsPackage());
+              packages.add(new RNFirebaseCrashlyticsPackage());
+              packages.add(new RNFirebaseAnalyticsPackage());
+              packages.add(new GomobilePackage());
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // packages.add(new MyReactNativePackage());
+              // Add unimodules
+              List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+                new ModuleRegistryAdapter(mModuleRegistryProvider)
+              );
+              packages.addAll(unimodules);
+              return packages;
+            }
+
+            @Override
+            protected String getJSMainModuleName() {
+              return "index";
+            }
+
+            @Override
+            protected String getJSBundleFile() {
+              return CodePush.getJSBundleFile();
+            }
+          };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    long size = 200L * 1024L * 1024L; // 200 MB
+    com.facebook.react.modules.storage.ReactDatabaseSupplier.getInstance(getApplicationContext()).setMaximumSize(size);
+  }
+}
